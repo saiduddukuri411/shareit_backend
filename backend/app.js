db49d9aaf345d218df686cf940f348861c85a164
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-09 03:25:55 -0400

    updated allowence

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 app.use('/uploads/images',express.static(path.join('uploads','images')))
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader(
-    "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods",'GET,POST,PATCH,DELETE');
