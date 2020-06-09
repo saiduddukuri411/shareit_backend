b0c71b1ed84b587d0cc7c4ee1d3bf780fbf14fc8
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-09 03:35:36 -0400

    got back

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ app.use(bodyParser.json());
 app.use('/uploads/images',express.static(path.join('uploads','images')))
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
