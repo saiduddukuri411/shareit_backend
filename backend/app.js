cd21fa7e541c0620dd1e53afb27d57aa1c69061c
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-09 04:01:44 -0400

    updated headers

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const { router } = require("./Routes/places-route");
-const path=require('path')
+const path = require("path");
 const mongoose = require("mongoose");
-const fs=require('fs');
+const fs = require("fs");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,15 +11,15 @@ const usersroute = require("./Routes/users-rote");
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/uploads/images',express.static(path.join('uploads','images')))
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
-  res.setHeader("Access-Control-Allow-Methods",'GET,POST,PATCH,DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
 
   next();
 });
@@ -31,10 +31,9 @@ app.use((req, res, next) => {
   throw error;
 });
 app.use((error, req, res, next) => {
-
-  if(req.file){
-    fs.unlink(req.file.path,(err)=>{
-      console.log(err)
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
     });
   }
   if (res.headerSent) {
@@ -43,8 +42,7 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occured" });
 });
-const url =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-rbtfy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-rbtfy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
   .connect(url)
