5140f8c3ad8b8e29086a74d6db6bf9bdfc7aaa0a
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-09 03:28:53 -0400

    double added

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader(
+    "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods",'GET,POST,PATCH,DELETE');
