b023105a47e03ee9821f8e59e1ae1ea961c591bb
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-10 03:24:07 -0400

    added corps

--- backend/app.js
+++ backend/app.js
@@ -13,17 +13,35 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/uploads/images',express.static(path.join('uploads','images')))
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", '*');
-  // response.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods",'GET,POST,PATCH,DELETE,HEAD');
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", '*');
+//   // response.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods",'GET,POST,PATCH,DELETE,HEAD');
 
-  next();
-});
+//   next();
+// });
+////
+
+var whitelist = ['https://shareit-2ff49.web.app/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Then pass them to cors:
+app.use(cors(corsOptions));
+
+
+/////
 
 app.use("/api/places", router);
 app.use("/api/users", usersroute);
