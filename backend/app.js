7ce001547eedc9dbae1b50a4ff3adda2d0bd1eeb
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-10 03:35:34 -0400

    changed corse

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const { router } = require("./Routes/places-route");
-const path=require('path')
+const path = require("path");
 const mongoose = require("mongoose");
-const fs=require('fs');
-const cors=require('cors');
+const fs = require("fs");
+const cors = require("cors");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,35 +11,34 @@ const usersroute = require("./Routes/users-rote");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // response.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,HEAD");
+
+  next();
+});
 app.use(bodyParser.json());
-app.use('/uploads/images',express.static(path.join('uploads','images')))
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", '*');
-//   // response.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods",'GET,POST,PATCH,DELETE,HEAD');
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+//
 
-//   next();
-// });
-////
+// var whitelist = ['https://shareit-2ff49.web.app/']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-var whitelist = ['https://shareit-2ff49.web.app/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-// Then pass them to cors:
-app.use(cors(corsOptions));
-
+// // Then pass them to cors:
+// app.use(cors(corsOptions));
 
 /////
 
@@ -50,10 +49,9 @@ app.use((req, res, next) => {
   throw error;
 });
 app.use((error, req, res, next) => {
-
-  if(req.file){
-    fs.unlink(req.file.path,(err)=>{
-      console.log(err)
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
     });
   }
   if (res.headerSent) {
@@ -62,8 +60,7 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occured" });
 });
-const url =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-rbtfy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-rbtfy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
   .connect(url)
