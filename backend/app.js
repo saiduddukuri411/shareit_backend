14e06c5290afafef082489764d37cc1211d9aa92
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-10 04:10:55 -0400

    get cors errors

--- backend/app.js
+++ backend/app.js
@@ -10,20 +10,24 @@ const HttpError = require("./models/http_errors");
 const usersroute = require("./Routes/users-rote");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // response.setHeader("Access-Control-Allow-Credentials", "true");
+  // response.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,HEAD");
+  if (re1.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,HEAD,PUT");
+  }
 
   next();
 });
-app.use(bodyParser.json());
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
 //
 
 // var whitelist = ['https://shareit-2ff49.web.app/']
